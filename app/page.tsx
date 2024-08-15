e44ac9f3d6b19da42d7a06dc35cd822ce6677010
errondo <abdusalim543@gmail.com>
2024-08-15 21:11:01 +0300

    cards

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   }, [api])
   return (
     <main>
-      <NavigationMenu className="">
+      <NavigationMenu className="border-b-[2px] border-s-slate-50 ">
         <Image priority src={logo} alt='logo' className="aspect-4/1 h-auto w-[90px]"/>
         <NavigationMenuList>
         <NavigationMenuItem key='1'>
@@ -95,7 +95,7 @@ export default function Home() {
                 <li key={'1'} className="">
                   <NavigationMenuLink >
                   <p className="flex">
-                    Upcoming  <svg xmlns="http://www.w3.org/2000/svg" className="icon w-6 h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" strokeLinejoin="round">
+                    Upcoming  <svg xmlns="http://www.w3.org/2000/svg" className="icon stroke-black dark:stroke-white w-6 h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                       <path d="M12 3a9 9 0 1 0 0 18a9 9 0 0 0 0 -18" />
                       <path d="M16 12l-4 -4" />
@@ -108,7 +108,7 @@ export default function Home() {
                 <li key={'2'}>
                   <NavigationMenuLink>
                   <p className="flex">
-                    Available  <svg xmlns="http://www.w3.org/2000/svg" className="icon w-6 h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" strokeLinejoin="round">
+                    Available  <svg xmlns="http://www.w3.org/2000/svg" className="icon stroke-black dark:stroke-white w-6 h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                       <path d="M12 3a9 9 0 1 0 0 18a9 9 0 0 0 0 -18" />
                       <path d="M16 12l-4 -4" />
@@ -121,7 +121,7 @@ export default function Home() {
                 <li key={'3'}>
                   <NavigationMenuLink>
                   <p className="flex">
-                    Classic Titles  <svg xmlns="http://www.w3.org/2000/svg" className="icon w-6 h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" strokeLinejoin="round">
+                    Classic Titles  <svg xmlns="http://www.w3.org/2000/svg" className="icon w-6 stroke-black dark:stroke-white h-6 " width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                       <path d="M12 3a9 9 0 1 0 0 18a9 9 0 0 0 0 -18" />
                       <path d="M16 12l-4 -4" />
@@ -152,7 +152,10 @@ export default function Home() {
           <NavigationMenuItem key='6'>
             <Link href="/news" legacyBehavior passHref>
               <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-white`}>
-                News
+                News<span className="relative -translate-y-2 flex h-3 w-3">
+  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+  <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+</span>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
@@ -177,20 +180,31 @@ export default function Home() {
       <CarouselNext className="absolute top-1/2" />
       <p className="absolute w-24 rounded-2xl p-5 bg-slate-600 -translate-y-8 left-1/2 right-1/2 -translate-x-1/2">{current} of {count}</p>
       </Carousel>
-      <section className="bg-slate-800 h-[2000px] w-full">
-         <Item>
-           {Array.from({length:11}).map((_,ind)=>
-                   <div key={ind} id={`${ind}`}>
-                   <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler bg-slate-950 icon-tabler-brand-apple-arcade" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" strokeLinejoin="round">
-             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-             <path d="M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-             <path d="M20 12.5v4.75a.734 .734 0 0 1 -.055 .325a.704 .704 0 0 1 -.348 .366l-5.462 2.58a5 5 0 0 1 -4.27 0l-5.462 -2.58a.705 .705 0 0 1 -.401 -.691l0 -4.75" />
-             <path d="M4.431 12.216l5.634 -2.332a5.065 5.065 0 0 1 3.87 0l5.634 2.332a.692 .692 0 0 1 .028 1.269l-5.462 2.543a5.064 5.064 0 0 1 -4.27 0l-5.462 -2.543a.691 .691 0 0 1 .028 -1.27z" />
-             <path d="M12 7l0 6" />
-           </svg>
-                   </div>
-                   )}
-         </Item>
+      <section className="dark:bg-slate-800 bg-slate-400 h-[1500px] w-full">
+         <div className="flex my-0 mx-auto items-center justify-center">
+           <div className="grid my-20 grid-cols-4 gap-8">
+             {Array.from({length:11}).map((_,ind)=>
+                     <div className="" key={ind} id={`${ind}`}>
+                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler bg-slate-950 icon-tabler-brand-apple-arcade" width="200" height="200" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" strokeLinejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+               <path d="M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+               <path d="M20 12.5v4.75a.734 .734 0 0 1 -.055 .325a.704 .704 0 0 1 -.348 .366l-5.462 2.58a5 5 0 0 1 -4.27 0l-5.462 -2.58a.705 .705 0 0 1 -.401 -.691l0 -4.75" />
+               <path d="M4.431 12.216l5.634 -2.332a5.065 5.065 0 0 1 3.87 0l5.634 2.332a.692 .692 0 0 1 .028 1.269l-5.462 2.543a5.064 5.064 0 0 1 -4.27 0l-5.462 -2.543a.691 .691 0 0 1 .028 -1.27z" />
+               <path d="M12 7l0 6" />
+             </svg>
+             <div className=" -translate-y-10 p-3 bg-gradient-to-t from-white to-transparent from-85% dark:from-black dark:to-transparent dark:from-85% dark:bg-gradient-to-t">
+              <h3 className="text-center">game title</h3>
+              <p>decription</p>
+              <span className="flex">4.5<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-star-filled" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffec00" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" stroke-width="0" fill="currentColor" />
+</svg></span><p className="text-right -translate-y-6">price: 9.99$</p>
+<Button className="mx-9 my-5" variant={'outline'}>purchase</Button>
+             </div>
+                     </div>
+                     )}
+           </div>
+         </div>
       </section>
       <footer className="w-full flex justify-between bg-slate-700 h-[200px]">
         <Image className="aspect-4/1 w-[200px]" src={logo} alt={'logo'} />
